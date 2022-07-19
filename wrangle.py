e823652a8f04ef3e9b1dd6a3bd81c8e595eb3369
Mijail Q Mariano <mijail.mariano@gmail.com>
2022-07-19 14:28:40 -0400

    191400JUL22 updates

--- wrangle.py
+++ wrangle.py
@@ -11,6 +11,10 @@ import matplotlib.pyplot as plt
 import env
 from env import user, password, host, get_connection
 
+# sklearn train, test, and split function
+from sklearn.model_selection import train_test_split
+
+
 
 def get_zillow_dataset():
     # importing "cached" dataset
@@ -46,10 +50,33 @@ def get_zillow_dataset():
 # focus is dropping Null values and changing column types 
 
 def clean_zillow_dataset(df):
-    # dropping null values in dataset (where <=2% makeup nulls in ea. feature/column)
+    # dropping null values in dataset (where <=1% makeup nulls in ea. feature/column)
     df = df.dropna()
 
     # converting "bedroom_count" "year_built", and "fips" columns to int type
-    df[["bedroom_count", "year_built", "fips"]] = df[["bedroom_count", "year_built", "fips"]].astype("int")
+    df["bedroom_count"] = df["bedroom_count"].astype("int").round()
+    df["year_built"] = df["year_built"].astype("int")
+    df["fips"] = df["fips"].astype("int")
+
     # lastly, return the cleaned dataset
     return df
+
+# function for handling outliers in the dataset
+def zillow_outliers(df):
+    df = df[df["bath_count"] <= 6]
+    df = df[df["bedroom_count"] <= 6]
+    df = df[df["finished_sq_feet"] <= 8000]
+    df = df[df["tax_dollar_value"] <= 1_500_000]
+
+    return df
+
+def train_validate_test_split(df):
+    train_and_validate, test = train_test_split(
+    df, test_size=0.2, random_state=123)
+    
+    train, validate = train_test_split(
+        train_and_validate,
+        test_size=0.3,
+        random_state=123)
+
+    return train, validate, test
